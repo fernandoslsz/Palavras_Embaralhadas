cd2dd65a8ef9338033ae100ff1b85a4347bf2c7c
fernando <slszfernando@gmail.com>
2022-08-16 15:41:04 -0300

    Eu adicionei um for loop

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 const initgame = () => {
-    let randomObj = words[Math.floor(Math.random() * words.length)]
-    console.log(randomObj)
+    let randomObj = words[Math.floor(Math.random() * words.length)];
+    let wordArray = randomObj.word.split("");
+    console.log(wordArray)
+    for ( let i = wordArray.length - 1; i > 0; i--) {
+        console.log(i)
+
+    }
 }
 
 initgame();

e8d55bb2e41d0e1e3ff3bd102687bb2ab6954804
fernando <slszfernando@gmail.com>
2022-08-16 17:26:19 -0300

    Adicionei a linha de código para embaralhar as letras

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 const initgame = () => {
-    let randomObj = words[Math.floor(Math.random() * words.length)];
-    let wordArray = randomObj.word.split("");
-    console.log(wordArray)
+    let randomObj = words[Math.floor(Math.random() * words.length)]; // pegando um objeto aleatório da variável words
+    let wordArray = randomObj.word.split(""); // separando cada letra da palavra aleatória
     for ( let i = wordArray.length - 1; i > 0; i--) {
-        console.log(i)
+        let j = Math.floor(Math.random() * (i + 1)); // pegando um número aleatório
+        [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];// embaralhando e trocando as letras aleatórias da variável wordArray
 
     }
+    
 }
 
 initgame();

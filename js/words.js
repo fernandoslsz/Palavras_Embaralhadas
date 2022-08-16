e3f8a35e9b225a16a2b3cf127f996b1051c86b6c
fernando <slszfernando@gmail.com>
2022-08-16 14:43:27 -0300

    Adicionei a função words

---
+++ js/words.js
@@ -0,0 +1,15 @@
+const words = [
+    {
+        word: "addition",
+        hint: "The process of adding number"
+
+    },
+    {
+        word: "meeting",
+        hint: "Event in which people come together"
+    },
+    {
+        word: "number",
+        hint: "Math symbol used for counting"
+    }
+]
